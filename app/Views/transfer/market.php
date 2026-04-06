c90ac744c0ab146f2d89eb322bac245f71cebb70
Farhood106 <farhood1988@gmail.com>
2026-04-06 13:35:59 +0330

    Create market.php

---
+++ app/Views/transfer/market.php
@@ -0,0 +1,37 @@
+<?php require_once __DIR__ . '/../layout/header.php'; ?>
+
+<div class="card">
+    <h2>بازیکنان در لیست انتقال</h2>
+
+    <?php if (empty($players)): ?>
+        <p>بازیکن در لیست انتقال وجود ندارد.</p>
+    <?php else: ?>
+        <table class="table">
+            <tr>
+                <th>نام</th>
+                <th>پست</th>
+                <th>قدرت</th>
+                <th>قیمت</th>
+                <th>خرید</th>
+            </tr>
+
+            <?php foreach ($players as $p): ?>
+            <tr>
+                <td><?= htmlspecialchars($p['name']) ?></td>
+                <td><?= htmlspecialchars($p['position']) ?></td>
+                <td><?= $p['overall'] ?></td>
+                <td><?= number_format($p['price']) ?> $</td>
+                <td>
+                    <form method="POST" action="/transfer/bid" data-ajax>
+                        <input type="hidden" name="player_id" value="<?= $p['id'] ?>">
+                        <input type="number" name="amount" value="<?= $p['price'] ?>" style="width:100px;">
+                        <button class="btn btn-success">پیشنهاد</button>
+                    </form>
+                </td>
+            </tr>
+            <?php endforeach; ?>
+        </table>
+    <?php endif; ?>
+</div>
+
+<?php require_once __DIR__ . '/../layout/footer.php'; ?>
